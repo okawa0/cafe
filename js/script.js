6ebbb1cd18d552db65b38883ccfcee4b59dadf8e
okawa <interplan.okawa@gmail.com>
2024-09-25 10:13:44 +0900

    googlemapsali

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 function initMap() {
     var MyLatLng = new google.maps.LatLng(35.57932968464123, 139.66212162368495);
     var Options = {
@@ -7,6 +8,8 @@ function initMap() {
     };
     var map = new google.maps.Map(document.getElementById('map'), Options);
 }
+
+
 
 const images = document.querySelectorAll('#mv img');
 let currentIndex = 0;
@@ -18,3 +21,23 @@ function changeImage() {
 }
 
 setInterval(changeImage, 5000); // 5秒ごとに切り替え
+
+$(function() {
+  var topBtn = $('#page-top');    
+  topBtn.hide();
+  
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      topBtn.fadeIn();
+    } else {
+      topBtn.fadeOut();
+    }
+  });
+  
+  topBtn.click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 500);
+    return false;
+  });
+});
